7422e6331c7546fdbb2709f88b51c0c04c8f8849
GitGeCyber <56102664+GitGeCyber@users.noreply.github.com>
2019-10-08 19:34:00 +0800

    Create practice.java

---
+++ practice.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class practice
+{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    
+    int x, y, sum;
+    
+    System.out.println("Input x : ");
+    x = sc.nextInt();
+    
+     System.out.println("Input x : ");
+    y = sc.nextInt();
+    
+    sum = x+y;
+ 
+    System.out.println("Sum is " +sum);
+    }
+}    
