1d4bdb4bb661e127f0092af996015faa5e5ac7fb
GitGeCyber <56102664+GitGeCyber@users.noreply.github.com>
2019-10-08 14:21:57 +0800

    Update prac.java

--- prac.java
+++ prac.java
@@ -2,7 +2,8 @@ public class prac
 {
   public static void main(String[]args)
     {
-        System.out.println("Practice : ")'
+        System.out.println("Practice : ");
+        System.exit(0);
     }
-    System.exit(0);
+    
 }    
